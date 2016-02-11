08d6efa3c12d66ca7ebefce9de4ab811b1d5aa02
mcflugen <huttone@colorado.edu>
2016-02-11 13:31:05 -0700

    Added RTI file reader, scalar to RTG grid converter.

--- topoflow_utils/hook.py
+++ topoflow_utils/hook.py
@@ -1,5 +1,8 @@
 """Routines used by WMT hooks for TopoFlow components."""
 import string
+import yaml
+
+import numpy as np
 
 
 choices_map = {
@@ -61,9 +64,61 @@ def assign_parameters(env, file_list):
     for key in env.copy().iterkeys():
         if key.endswith(terminator):
             key_root, sep, end = key.partition(terminator)
-            if env[key] == 'Scalar':
-                env[key_root] = env[key_root + '_scalar']
-            else:
+            # if env[key] == 'Scalar':
+            #     env[key_root] = env[key_root + '_scalar']
+            # else:
+            if env[key] != 'Scalar':
                 env[key_root] = env[key_root + '_file']
                 file_list.append(key_root)
             env[key_root + '_dtype'] = get_dtype(env[key_root])
+
+
+def load_rti(name):
+    """Load a RiverTools RTI file.
+    
+    Parameters
+    ----------
+    name : string
+        Name of the RTI file.
+    
+    Returns
+    -------
+    dict
+        Key-value pairs given in the RTI file.
+    """
+    lines = []
+    with open(name, 'r') as fp:
+        for line in fp:
+            if ';' in line:
+                line = line[:line.find(';')]
+            if ':' in line:
+                lines.append(line)
+
+    return yaml.load(os.linesep.join(lines))
+
+
+def scalar_to_rtg_file(name, env):
+    """Convert a scalar value to an RiverTools RTG file.
+    
+    Parameters
+    ----------
+    name : string
+        Name of the variable to convert to a grid.
+    env : dict
+        Mapping of keys to values for the parameter file environment.
+    """
+    env[name + '_ptype'] = 'Grid'
+    env[name + '_dtype'] = 'string'
+    file_name = env['case_prefix'] + '_{name}.rtg'.format(name=name)
+
+    rti = load_rti(env['site_prefix'] + '.rti')
+    shape = (rti['Number of rows'], rti['Number of columns'])
+    byte_order = rti['Byte order']
+    if byte_order == 'MSB':
+        dtype = '>f4'
+    else:
+        dtype = '<f4'
+    grid = np.full(shape, env[name], dtype=dtype)
+    grid.tofile(file_name)
+
+    env[name] = env[name + '_file'] = file_name
